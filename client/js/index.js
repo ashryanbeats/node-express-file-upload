8399426a6b719a61fa558bdc8b57a9cf12101e16
Ash <ash@ashryanbeats.com>
2017-03-22 18:25:55 -0400

    Sending request via xhr.

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,32 @@
-const fileInput = document.querySelector("#file-input");
+const form = document.querySelector("#form");
+const status = document.querySelector("#status");
 
-fileInput.addEventListener("change", function(e) {
-  
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.set("pic", form.children["file-input"].files[0]);
+
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.open("POST", "/api/upload");
+  httpRequest.send(formData);
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        form.reset();
+        status.textContent = "Uploaded!";
+        clearTextContent();
+      }
+      else {
+        console.log(httpRequest.status);
+        status.textContent = `Problem: ${httpRequest.status}`;
+        clearTextContent();
+      }
+    }
+  }
 });
+
+function clearTextContent() {
+  setTimeout(() => status.textContent = "", 2000);
+}
